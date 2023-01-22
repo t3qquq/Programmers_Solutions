d30a831c39a43b3a7da6860e85d2a9658c610686
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-01-22 20:54:59 +0900

    feat: 120853 JS 풀이 추가

---
+++ pengooseDev/120853/JS/pengooseDev.js
@@ -0,0 +1,10 @@
+function solution(s) {
+  let answer = 0;
+  const myArr = s.split(' ').reduce((prev, curr) => {
+    if (curr === 'Z') answer -= Number(prev);
+    if (curr !== 'Z') answer += Number(curr);
+
+    return curr;
+  }, s.split(' ')[0]);
+  return answer;
+}
---
+++ pengooseDev/120853/readme.md
@@ -0,0 +1,3 @@
+## 컨트롤 제트
+
+### [> Link](https://school.programmers.co.kr/learn/courses/30/lessons/120853)
