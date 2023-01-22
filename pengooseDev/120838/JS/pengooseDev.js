34cd0e0a3d8f43bb18a9f551f619ee460927ef92
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-01-22 16:29:04 +0900

    feat: 120838 JS 풀이 추가

---
+++ pengooseDev/120838/JS/pengooseDev.js
@@ -0,0 +1,36 @@
+function solution(letter) {
+  const letterArr = letter.split(' ');
+  const answer = [];
+  for (let i of letterArr) answer.push(morse[i]);
+
+  return answer.join('');
+}
+
+const morse = {
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+};
---
+++ pengooseDev/120838/readme.md
@@ -0,0 +1,3 @@
+## 모스부호 (1)
+
+### [> Link](https://school.programmers.co.kr/learn/courses/30/lessons/120838)
